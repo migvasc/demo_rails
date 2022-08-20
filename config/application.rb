e0706fa90954bb002ed28aabcf3e09d1b688f6a3
Miguel Vasconcelos <miguel.vasconcelos@usp.br>
2022-08-20 22:34:18 +0000

    add dominio heroku

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module TESTE
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.hosts << 'www.example.com'
+    config.hosts << 'demorails2022.herokuapp.com'
 
     
 
