be01cf8dbc5e45ad5ab6f7b96dd0f2ddbd309f4b
Ubuntu <ubuntu@ip-172-31-32-224.ec2.internal>
2022-08-20 18:47:33 +0000

    big bang

---
+++ config/application.rb
@@ -0,0 +1,24 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module TESTE
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.0
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << "360e4a16f7ad409085652ea86fe022ee.vfs.cloud9.us-east-1.amazonaws.com"
+
+  end
+end
